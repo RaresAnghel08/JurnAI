126379f7e1e82f0a9ae1344cd605879db9232ad9
Rares08 <raresanghel2008@gmail.com>
2025-07-16 20:15:54 +0300

    Update model version from gpt-4 to gpt-3.5-turbo in chat completion request

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ Scrie fiecare punct clar, cu titlu:
 
     try:
         response = client.chat.completions.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.7
         )
